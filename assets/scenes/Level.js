81b1f17252dd526c6561a61eb61ed7b90a1f6fba
Matt Welsh <mdw@mdw.la>
2021-04-23 20:38:53 -0700

    Working on keys

--- assets/scenes/Level.js
+++ assets/scenes/Level.js
@@ -4,7 +4,10 @@ console.log("MDW: Loading Level.js");
 var player;
 var cursors;
 var walls;
-
+var keyA;
+var keyS;
+var keyD;
+var keyW;
 
 /* START OF COMPILED CODE */
 
@@ -67,28 +70,45 @@ class Level extends Phaser.Scene {
     platform.body.allowGravity = false;
     platform.play('platform');
 
-    cursors = this.input.keyboard.createCursorKeys();
+    //cursors = this.input.keyboard.createCursorKeys();
+    keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
 
     // Add player
     player = this.physics.add.sprite(69, 100, 'character');
-    //player.scaleX = 5;
-    //player.scaleY = 5;
     player.setBounce(0.2); // our player will bounce from items
     player.setCollideWorldBounds(true); // don't go out of the map
+
+    this.cameras.main.setBounds(0, 0, 400, 300);
+    this.cameras.main.startFollow(player);
+    this.cameras.main.setBackgroundColor('#ccccff');
   }
 
   update() {
     this.physics.collide(player, walls);
+    if(keyA.isDown) {
+      console.log('A key pressed')
+    } else if(keyS.isDown) {
+      console.log('S key pressed')
+    } else if(keyD.isDown) {
+      console.log('D key pressed')
+    } else if(keyW.isDown) {
+      console.log('W key pressed')
+    }
 
+    /*
     if (cursors.left.isDown) {
-      player.setVelocityX(-300);
+      player.setVelocityX(-30);
     } else if (cursors.right.isDown) {
-      player.setVelocityX(300);
-    } else if (cursors.up.isDown && player.body.onFloor()) {
-      player.setVelocityY(-500);
+      player.setVelocityX(30);
+    } else if ((cursors.space.isDown || cursors.up.isDown) && player.body.onFloor()) {
+      player.setVelocityY(-50);
     } else {
       player.setVelocityX(0);
     }
+    */
   }
 
   /* END-USER-CODE */
